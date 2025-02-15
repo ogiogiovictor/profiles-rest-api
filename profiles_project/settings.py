8ea7c380f348166746630025d393f9ee8a188fb5
Victor Ogiogio <victor.ogiogio@ibedc.com>
2025-02-15 04:35:39 +0100

    Added User Profile

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -124,3 +124,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'profiles_api.UserProfile' # for activating custom user profile instead of django.db.models user profile
+
+
